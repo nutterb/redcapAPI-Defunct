8e9dac05048c0edabed83e036b13ff2f40dc7d1f
nutterb <benjamin.nutter@gmail.com>
2013-10-11 09:02:45 -0400

    Testing the functions without parsing metadata.

--- R/downloadRedcap.R
+++ R/downloadRedcap.R
@@ -30,24 +30,24 @@ downloadRedcap <- function(APIKEY, url=options()$redcap_api_url,
   }
   
   # Make a list of calls to create factors
-  levelList <- function(r){
-    switch(gg$field_type[r],
-           checkbox = {choices <- unlist(str_split(gg$select_choices_or_calculations[r], "[|]"))
-                       choices <- str_split_fixed(choices, ", ", 2)
-                       nums <- str_trim(choices[, 1])
-                       choices <- str_c("c('", str_c(str_trim(choices[,2]), collapse="', '"), "')", sep="")
-                       return(sprintf('data$%1$s___%2$s <- factor(data$%1$s___%2$s, c("0", "1"), c("Unchecked", "Checked"))', 
-                                      gg$field_name[r], nums))
-           },
-           radio = return(sprintf('attr(data$%s, "redcapLevels") <- NULL', gg$field_name[r])),
-           dropdown = return(sprintf('attr(data$%s, "redcapLevels") <- NULL', gg$field_name[r])),
-           yesno = return( sprintf('data$%1$s <- factor(data$%1$s, levels=c("0", "1"), labels=c("No", "Yes"))', gg$field_name[r])),
-           truefalse = return(sprintf('data$%1$s <- factor(data$%1$s, levels=c("0", "1"), labels=c("FALSE", "TRUE"))', gg$field_name[r])),
-           notes = return(""),
-           text = return(""),
-           slider = return(""),
-           sprintf("print(\"I don\'t know field_type \'%s\' for field \'%s\'\")", gg$field_type[r], gg$field_name[r]))
-  }
+  #levelList <- function(r){
+  #  switch(gg$field_type[r],
+  #         checkbox = {choices <- unlist(str_split(gg$select_choices_or_calculations[r], "[|]"))
+  #                     choices <- str_split_fixed(choices, ", ", 2)
+  #                     nums <- str_trim(choices[, 1])
+  #                     choices <- str_c("c('", str_c(str_trim(choices[,2]), collapse="', '"), "')", sep="")
+  #                     return(sprintf('data$%1$s___%2$s <- factor(data$%1$s___%2$s, c("0", "1"), c("Unchecked", "Checked"))', 
+  #                                    gg$field_name[r], nums))
+  #         },
+  #         radio = return(sprintf('attr(data$%s, "redcapLevels") <- NULL', gg$field_name[r])),
+  #         dropdown = return(sprintf('attr(data$%s, "redcapLevels") <- NULL', gg$field_name[r])),
+  #         yesno = return( sprintf('data$%1$s <- factor(data$%1$s, levels=c("0", "1"), labels=c("No", "Yes"))', gg$field_name[r])),
+  #         truefalse = return(sprintf('data$%1$s <- factor(data$%1$s, levels=c("0", "1"), labels=c("FALSE", "TRUE"))', gg$field_name[r])),
+  #         notes = return(""),
+  #         text = return(""),
+  #         slider = return(""),
+  #         sprintf("print(\"I don\'t know field_type \'%s\' for field \'%s\'\")", gg$field_type[r], gg$field_name[r]))
+  #}
   
   
   # <instrument>_complete
@@ -59,15 +59,16 @@ downloadRedcap <- function(APIKEY, url=options()$redcap_api_url,
                                                                            complete[complete %in% gg$field_names])
   }
   
-  if (!raw){
-    levels <- lapply(1:nrow(gg), levelList)
-    if (any(complete %in% gg$field_names)) levels[[nrow(gg)+1]] <- 
-      sprintf('data$%1$s <- factor(data$%1$s, levels=c("2", "1", "0"), labels=c("Complete", "Unverified", "Incomplete"))', 
-              complete[complete %in% gg$field_names])
-  }
+  #if (!raw){
+  #  levels <- lapply(1:nrow(gg), levelList)
+  #  if (any(complete %in% gg$field_names)) levels[[nrow(gg)+1]] <- 
+  #    sprintf('data$%1$s <- factor(data$%1$s, levels=c("2", "1", "0"), labels=c("Complete", "Unverified", "Incomplete"))', 
+  #            complete[complete %in% gg$field_names])
+  #}
   
   
-  info <- c(if (!raw) unlist(levels) else "", if (label) unlist(labels) else "")
+  #info <- c(if (!raw) unlist(levels) else "", if (label) unlist(labels) else "")
+  info <- unlist(labels)
   # download data
   data <- exportRecords(rcon, factors=!raw, fields=fields, forms=forms, records=records, events=events)
   # apply labels and levels
