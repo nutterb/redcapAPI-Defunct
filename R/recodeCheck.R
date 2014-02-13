86de2c60f17c2f807805eefac8d610f4b14ea902
nutterb <benjamin.nutter@gmail.com>
2014-02-13 16:18:42 -0500

    Create recodeCheck.R
    Function recodes "Checked"/"Unchecked" fields to "Yes"/"No" or some other user specified arrangement.

---
+++ R/recodeCheck.R
@@ -0,0 +1,31 @@
+recodeCheck <- function(df, vars, 
+                        levels=c("Unchecked", "Checked"), labels=c("No", "Yes"), 
+                        reverse=FALSE){
+  #* reverse the labels.  
+  if (reverse){
+    levels <- rev(levels)
+    labels <- rev(labels)
+  }
+  
+  #* If not variable names are provided, check the data frame for all variables in which all values 
+  #* are either "Checked" or "Unchecked"
+  if (missing(vars)){
+    checkbox <- sapply(df, function(x) all(x %in% levels))
+  }
+  
+  #* If variable names are given, ensure that they are checkbox variables.  Ignore them if anything
+  #* other than "Checked" or "Unchecked" appears in the values.
+  else {
+    vars_are_check <- sapply(df, function(x) all(x %in% levels))
+    vars_not_check <- vars[!vars_are_check]
+    if (any(!vars_are_check)) warning(paste("'", paste(vars[!vars_are_check], collapse = "', '"), 
+                                            "' do not appear to be 'checkbox' variables.",
+                                            "\nThese variables were not recoded.", sep=""))
+    checkbox <- vars[vars_are_check]
+  }
+  
+  #* Apply the new labels
+  df[checkbox] <- lapply(df[checkbox], factor, levels=levels, labels=labels)
+  return(df)
+}
+  
