43fe0dbf4ff3d931bfc3a9d5fcc50c82bca0f9e4
Benjamin <benjamin.nutter@gmail.com>
2014-07-16 10:08:08 -0400

    Update fieldToVar.R

--- R/fieldToVar.R
+++ R/fieldToVar.R
@@ -19,12 +19,12 @@ fieldToVar <-
     
     #* Time (HH:MM)
     if (grepl("time_mm_ss", m$text_validation_type_or_show_slider_number) && dates){
-      d <- times(paste("00:", d, sep=""), format=c(times="h:m:s"))
+      d <- times(ifelse(!is.na(d), paste("00:", d, sep=""), d), format=c(times="h:m:s"))
     }
     
-    #* Time (HH:MM)
+    #* Time (MM:SS)
     if ("time" %in% m$text_validation_type_or_show_slider_number && dates){
-      d <- times(paste(d, ":00", sep=""), format=c(times="h:m:s"))
+      d <- times(ifelse(!is.na(d), paste(d, ":00", sep=""), d), format=c(times="h:m:s"))
     }
     
     # Continuous variables
