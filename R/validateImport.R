c99b48562de886783fdbe6aad9a52ee52be0f4c6
Benjamin <benjamin.nutter@gmail.com>
2014-07-17 15:33:24 -0400

    Update validateImport.R

--- R/validateImport.R
+++ R/validateImport.R
@@ -216,7 +216,7 @@ validateImport <- function(field, meta_data, records, ids,
     x[x == "NA"] <- NA
 
     # reconvert to times objects to compare to meta data
-    x <- times(paste("00", x, sep=":"), format=c(times="h:m:s"))
+    x <- suppressWarnings(times(paste("00", x, sep=":"), format=c(times="h:m:s")))
     
     if (!is.na(meta_data$text_validation_min)){
       w <- which(x < times(paste("00", meta_data$text_validation_min, sep=":")) & !is.na(x))
@@ -238,7 +238,7 @@ validateImport <- function(field, meta_data, records, ids,
     
     # Convert to character again for upload.
     x <- as.character(x)
-    x <- sapply(strsplit(x, ":"), head, 2)
+    x <- sapply(strsplit(x, ":"), tail, 2)
     x <- sapply(x, paste, collapse=":")
     
     return(x)
@@ -261,7 +261,7 @@ validateImport <- function(field, meta_data, records, ids,
     x[x == "NA"] <- NA
     
     # reconvert to times objects to compare to meta data
-    x <- times(paste(x, "00", sep=":"), format=c(times="h:m:s"))
+    x <- suppressWarnings(times(paste(x, "00", sep=":"), format=c(times="h:m:s")))
     
     if (!is.na(meta_data$text_validation_min)){
       w <- which(x < times(paste(meta_data$text_validation_min, "00", sep=":")) & !is.na(x))
