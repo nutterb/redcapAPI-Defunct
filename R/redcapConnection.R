b693a955b31df37744bdae23a2468ae1fac0d7f1
Benjamin <benjamin.nutter@gmail.com>
2014-07-18 11:50:09 -0400

    Update redcapConnection.R

--- R/redcapConnection.R
+++ R/redcapConnection.R
@@ -1,9 +1,9 @@
 redcapConnection <-
 function(url=getOption('redcap_api_url'),token,conn,project)
 {
-   if (is.null(url) && missing(conn))
+   if (is.na(url) && missing(conn))
       stop("Need one of url or conn")
-   if (!is.null(url))
+   if (!is.na(url))
    {
       if (missing(token))
          stop("Need an API token")
