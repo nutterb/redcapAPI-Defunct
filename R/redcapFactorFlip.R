6624cc6fcb112e5eabdbb0c59477a1deada86a1e
Benjamin <benjamin.nutter@gmail.com>
2014-08-01 22:19:18 -0400

    Create redcapFactorFlip.R

---
+++ R/redcapFactorFlip.R
@@ -0,0 +1,26 @@
+redcapFactorFlip <- function(v){
+  #* extract attributes to be applied later
+  redcapLabels <- attributes(v)$redcapLabels
+  redcapLevels <- attributes(v)$redcapLevels
+  
+  #* labelled to coded
+  if ("factor" %in% class(v)){
+    v <- factor(as.character(v), 
+                redcapLabels, 
+                redcapLevels)
+    v <- as.character(v)
+    if (is.numeric(redcapLevels)) v <- as.numeric(v)
+  }
+  
+  #* coded to labelled
+  else{
+    v <- factor(v, 
+                attributes(v)$redcapLevels,
+                attributes(v)$redcapLabels)
+  }
+  
+  #* reapply attributes
+  attr(v, 'redcapLabels') <- redcapLabels
+  attr(v, 'redcapLevels') <- redcapLevels
+  return(v)
+}
