32fa52e7ba1763f5e44fcda8d9be04fbb8315663
Benjamin <benjamin.nutter@gmail.com>
2014-08-01 20:06:58 -0400

    Update deleteFiles.R

--- R/deleteFiles.R
+++ R/deleteFiles.R
@@ -12,14 +12,29 @@ deleteFiles.redcapApiConnection <- function(rcon, record, field, event, ...){
     stop("The arguments 'record', 'field', and 'event' may each only have length 1")
   }
   
+  #* make sure 'field' exist in the project and are 'file' fields
+  if (is.null(meta_data)) meta_data <- exportMetaData(rcon)
+  if (!field %in% meta_data$field_name) stop(paste("'", field, "' does not exist in the project.", sep=""))
+  if (meta_data$field_type[meta_data$field_name == field] != "file")
+      stop(paste("'", field, "' is not of field type 'file'", sep=""))
+      
+  #* make sure 'event' exists in the project
+  if (missing(event)) event <- ""
+  if (is.null(events_list)) events_list <- exportEvents(rcon)
+  if (!is.null(events_list)){
+    if (!event %in% events_list$unique_event_name) 
+      stop(paste("'", event, "' is not a valid event name in this project.", sep=""))
+  }
+  
   .params <- list(token=rcon$token, content='file',
                   action='delete', record=record,
                   field=field)
-  if (!missing(event)) .params[['event']] <- event
+  if (event == "") .params[['event']] <- event
   
   #* Delete the file
-  postForm(uri=rcon$url, .params=.params,
-           .opts=curlOptions(ssl.verifyhost=FALSE))
-  
-  message("The file was successfully deleted")
+  noFile <- tryCatch(postForm(uri=rcon$url, .params=.params,
+                           .opts=curlOptions(ssl.verifyhost=FALSE)),
+                  error = function(cond) if (grepl("Bad Request", cond[1])) return(TRUE))         
+  if (noFile) message("There is no file to delete")
+    else message("The file was successfully deleted")
 }
