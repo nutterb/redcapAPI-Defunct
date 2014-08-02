d940141df4654233bff4549cd5d09465970b44d2
Benjamin <benjamin.nutter@gmail.com>
2014-08-01 20:55:55 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,19 @@
 .onLoad <- function(libname,pkgname)
 {
-   options(redcap.dataTable='redcap_data')
-   options(redcap.metaDataTable='redcap_metadata')
-   options(redcap_api_url = NA)
-   options(redcap_project_info = list(meta_data = NULL,
-                               users = NULL,
-                               events = NULL,
-                               arms = NULL,
-                               mappings = NULL))
+  options(redcap.dataTable='redcap_data')
+  options(redcap.metaDataTable='redcap_metadata')
+  options(redcap_api_url = NA)
+  options(redcap_project_info = list(meta_data = NULL,
+                                     users = NULL,
+                                     events = NULL,
+                                     arms = NULL,
+                                     mappings = NULL))
 }
 
 .onUnload <- function(libPath)
 {
-   options(redcap.dataTable=NULL)
-   options(redcap.metaDataTable=NULL)
-   options(redcap_api_url=NULL)
-   options(redcap_project_info=NULL)
+  options(redcap.dataTable=NULL)
+  options(redcap.metaDataTable=NULL)
+  options(redcap_api_url=NULL)
+  options(redcap_project_info=NULL)
 }
