46c7e5a6e990dd2552be4dbe6e658069c35e76bf
Benjamin <benjamin.nutter@gmail.com>
2014-08-01 21:05:26 -0400

    Create zzz.R

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+.onLoad <- function(libname,pkgname)
+{
+   options(redcap.dataTable='redcap_data')
+   options(redcap.metaDataTable='redcap_metadata')
+   options(redcap_api_url = NA)
+}
+
+.onUnload <- function(libPath)
+{
+   options(redcap.dataTable=NULL)
+   options(redcap.metaDataTable=NULL)
+   options(redcap_api_url=NULL)
+}
