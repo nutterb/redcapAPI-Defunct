2ab242e98a4275dca167941ec93c829cc58a6f41
Benjamin <benjamin.nutter@gmail.com>
2014-08-01 21:38:40 -0400

    Update redcapProjectInfo.R

--- R/redcapProjectInfo.R
+++ R/redcapProjectInfo.R
@@ -5,7 +5,7 @@ redcapProjectInfo.redcapDbConnection <- function(rcon, date=TRUE, label=TRUE, ..
           "has not yet been written.  Please consider using the API.")
 }
 
-redcapProjectInfo.redcapApiConnection <- function(rcon, date=TRUE, label=TRUE){
+redcapProjectInfo.redcapApiConnection <- function(rcon, date=TRUE, label=TRUE, ...){
   options(redcap_project_info = list(meta_data = exportMetaData(rcon),
                                users = exportUsers(rcon, date, label),
                                events = exportEvents(rcon),
