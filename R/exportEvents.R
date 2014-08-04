c100f77d519f50b5a399c3e1998e2684cc082d9d
Benjamin <benjamin.nutter@gmail.com>
2014-08-03 22:43:03 -0400

    Update exportEvents.R

--- R/exportEvents.R
+++ R/exportEvents.R
@@ -12,5 +12,5 @@ exportEvents.redcapApiConnection <- function(rcon, arms, ...){
   x <- httr::POST(url=rcon$url, body=.params)
   if (x$status_code == "200")
     x <- read.csv(textConnection(as.character(x)), stringsAsFactors=FALSE)  
-  else(stop(as.character(x)))
+  else(stop(paste(x$status_code, ": ", as.character(x), sep="")))
 }
