51a92ebcd1b0bece2176895e0986d00dad116ac2
Benjamin <benjamin.nutter@gmail.com>
2014-08-05 15:10:20 -0400

    Update exportArms.R

--- R/exportArms.R
+++ R/exportArms.R
@@ -12,7 +12,7 @@ exportArms.redcapApiConnection <- function(rcon, arms, ...){
   
   if (x$status_code == "200")
     return(read.csv(textConnection(as.character(x)), stringsAsFactors=FALSE))
-  else if (x$status_code == "400" & as.character(x) == "You cannot export events for classic projects") 
+  else if (x$status_code == "400" & as.character(x) == "You cannot export arms for classic projects") 
     paste(x$status_code, ": ", as.character(x), sep="")
   else stop(paste(x$status_code, ": ", as.character(x), sep=""))
 }
