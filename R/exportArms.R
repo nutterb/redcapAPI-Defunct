1c176b48cfb00963dd41fe7a3072ab0855f2153f
Benjamin <benjamin.nutter@gmail.com>
2014-08-06 20:04:18 -0400

    Update exportArms.R

--- R/exportArms.R
+++ R/exportArms.R
@@ -12,6 +12,8 @@ exportArms.redcapApiConnection <- function(rcon, arms, ...){
   
   if (x$status_code == "200")
     return(read.csv(textConnection(as.character(x)), stringsAsFactors=FALSE))
+  #*** For classic projects, we want to avoid throwing a disruptive error. Instead, we 
+  #*** return the message that indicates this is a classic project.
   else if (x$status_code == "400" & as.character(x) == "You cannot export arms for classic projects") 
     paste(x$status_code, ": ", as.character(x), sep="")
   else stop(paste(x$status_code, ": ", as.character(x), sep=""))
