73ad6e4c3001ca5686af8a23955189756aa09468
Benjamin <benjamin.nutter@gmail.com>
2014-08-07 20:42:03 -0400

    Update exportInstruments.R

--- R/exportInstruments.R
+++ R/exportInstruments.R
@@ -1,11 +1,11 @@
-exportInstruments <- function(rcon) UseMethod("exportInstruments")
+exportInstruments <- function(rcon, ...) UseMethod("exportInstruments")
 
-exportInstruments.redcapDbConnection <- function(rcon){
+exportInstruments.redcapDbConnection <- function(rcon, ...){
   message("Please accept my apologies.  The exportInstruments method for redcapDbConnection objects\n",
           "has not yet been written.  Please consider using the API.")
 }
 
-exportInstruments.redcapApiConnection <- function(rcon){
+exportInstruments.redcapApiConnection <- function(rcon, ...){
   x <- httr::POST(url=rcon$url, body=list(token=rcon$token, content='instrument', format='csv'))
   if (x$status_code == "200")
     read.csv(textConnection(as.character(x)), stringsAsFactors=FALSE) 
