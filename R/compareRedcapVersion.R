26b773cb2b237604ef7d2d7700c961dacb5e4b91
Benjamin <benjamin.nutter@gmail.com>
2014-08-11 17:08:47 -0400

    Create compareRedcapVersion.R

---
+++ R/compareRedcapVersion.R
@@ -0,0 +1,20 @@
+compareRedcapVersion <- function(current_version, required_version){
+  #* Ultimately, a value of -1, 0, or 1 will be returned.
+  #* -1 indicates the current_version is lower than the required version
+  #* 0 indicates the versions are the same
+  #* 1 indicates the current version is ahead of the required version
+  #* In context of asking "is the current version at least as high as the required version
+  #*     an -1 means FALSE.  Any other answer is TRUE
+  
+  #* accept version numbers of the forms x, x.y, and x.y.z
+  check_current <- grepl("(\\d{1,3}|\\d{1,3}[.]\\d{1,3}|\\d{1,3}[.]\\d{1,3}[.]\\d{1,3})")
+  
+  #* If a version style is not recognized, set the current version to 'Version Unknown'
+  if (!check_current) current_version <- "Version Unknown"
+  
+  #* if current version is null or unknown, return -1
+  if (is.null(current_version) | current_version == "Version Unknown") return(-1)
+  
+  #* compare versions using the function in the utils package
+  else return(utils::compareVersion(current_version, required_version))
+}
