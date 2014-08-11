3214b15308dedec3667ded8eaa754649ca09d0a6
Benjamin <benjamin.nutter@gmail.com>
2014-08-11 16:08:53 -0400

    Update redcapProjectInfo.R

--- R/redcapProjectInfo.R
+++ R/redcapProjectInfo.R
@@ -1,14 +1,27 @@
-redcapProjectInfo <- function(rcon, date=TRUE, label=TRUE, ...) UseMethod("redcapProjectInfo")
+redcapProjectInfo <- function(rcon, date=TRUE, label=TRUE, 
+                              meta_data=TRUE, users=TRUE, events=TRUE,
+                              arms=TRUE, mappings=TRUE,
+                              version=TRUE, ...) UseMethod("redcapProjectInfo")
 
-redcapProjectInfo.redcapDbConnection <- function(rcon, date=TRUE, label=TRUE, ...){
+redcapProjectInfo.redcapDbConnection <- function(rcon, date=TRUE, label=TRUE, 
+                              meta_data=TRUE, users=TRUE, events=TRUE,
+                              arms=TRUE, mappings=TRUE,
+                              version=TRUE, ...){
   message("Please accept my apologies.  The redcapProjectInfo method for redcapDbConnection objects\n",
           "has not yet been written.  Please consider using the API.")
 }
 
-redcapProjectInfo.redcapApiConnection <- function(rcon, date=TRUE, label=TRUE, ...){
-  options(redcap_project_info = list(meta_data = exportMetaData(rcon),
-                               users = exportUsers(rcon, date, label),
-                               events = exportEvents(rcon),
-                               arms = exportArms(rcon),
-                               mappings = exportMappings(rcon)))
+redcapProjectInfo.redcapApiConnection <- function(rcon, date=TRUE, label=TRUE, 
+                              meta_data=TRUE, users=TRUE, events=TRUE,
+                              arms=TRUE, mappings=TRUE,
+                              version=TRUE, ...,
+                              v.number = ""){
+  proj <- list(meta_data = if (meta_data) exportMetaData(rcon) else NULL,
+               users = if (users) exportUsers(rcon, date, label) else NULL,
+               events = if (events) exportEvents(rcon) else NULL,
+               arms = if (arms) exportArms(rcon) else NULL,
+               mappings = if (mappings) exportMappings(rcon) else NULL,
+               version = if (version) exportVersion(rcon) else v.number)
+  class(proj) <- c("redcapProject", "list")
+  return(proj)
 }
