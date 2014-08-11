362a05c0678b274d99da055c8034775b05ccf738
Benjamin <benjamin.nutter@gmail.com>
2014-08-11 17:45:04 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
 {
   options(redcap.dataTable='redcap_data')
   options(redcap.metaDataTable='redcap_metadata')
-  options(redcap_api_url = NA))
+  options(redcap_api_url = NA)
 }
 
 .onUnload <- function(libPath)
