4accffe575d35ec0f077417e4a2ce9399137d59c
Benjamin <benjamin.nutter@gmail.com>
2014-08-11 17:51:45 -0400

    Update exportInstruments.R

--- R/exportInstruments.R
+++ R/exportInstruments.R
@@ -7,7 +7,7 @@ exportInstruments.redcapDbConnection <- function(rcon, ...){
 
 exportInstruments.redcapApiConnection <- function(rcon, ..., proj=NULL){
   v <- if (is.null(proj$version)) exportVersion(rcon) else proj$version
-  if (compareRedcapVersion(proj$version, "5.9.0") == -1) 
+  if (compareRedcapVersion(v, "5.9.0") == -1) 
     return("'exportInstruments' requires REDCap version 5.9.0 or higher")
   x <- httr::POST(url=rcon$url, body=list(token=rcon$token, content='instrument', format='csv'))
   if (x$status_code == "200")
