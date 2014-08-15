94fac9dc128f0fdb0bf79aa6a632f6fb97f83deb
Benjamin <benjamin.nutter@gmail.com>
2014-08-15 16:34:09 -0400

    stop missing event error

--- R/exportFiles.R
+++ R/exportFiles.R
@@ -14,6 +14,7 @@ exportFiles.redcapApiConnection <- function(rcon, record, field, event, dir, fil
   if (missing(dir)) dir <- getwd()
   
   #* stop the function if arguments do not specify a unique record-event
+  if (missing(event)) event <- ""
   if (any(sapply(list(record, field, event), length) > 1)){
     stop("The arguments 'record', 'field', and 'event' may each only have length 1")
   }
@@ -25,7 +26,6 @@ exportFiles.redcapApiConnection <- function(rcon, record, field, event, dir, fil
       stop(paste("'", field, "' is not of field type 'file'", sep=""))
       
   #* make sure 'event' exists in the project
-  if (missing(event)) event <- ""
   if (is.null(proj$events)) events_list <- exportEvents(rcon)
   if (class(events_list) == 'data.frame'){
     if (!event %in% events_list$unique_event_name) 
