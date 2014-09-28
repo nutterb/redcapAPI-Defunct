c2612e74a1d57adc5e45d05b9a20929f7e849cfd
Benjamin <benjamin.nutter@gmail.com>
2014-09-27 20:54:54 -0400

    Update apiCall.R

--- R/apiCall.R
+++ R/apiCall.R
@@ -2,8 +2,8 @@ apiCall <- function(url, body, config){
   x <- tryCatch(httr::POST(url=url, body=body, config=config),
                 error = function(cond){
                   if (grepl("GnuTLS recv error [(]-9[)]", cond)){
-                    m <- RCurl::postForm(uri=url, .params=body,
-                                         .opts=config)
+                    m <- httr::POST(url=url, body=body,
+                                         config=c(list(encoding='identity'), config))
                     m <- list(content=charToRaw(m),
                               status_code = "200",
                               url=url)
