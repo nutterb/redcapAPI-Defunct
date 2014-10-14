75496b1ea8f061a7d1371868e3a36926edc63366
Benjamin <benjamin.nutter@gmail.com>
2014-10-14 10:54:23 -0400

    Remove manipulations that had been used with RCurl

--- R/apiCall.R
+++ R/apiCall.R
@@ -4,10 +4,6 @@ apiCall <- function(url, body, config){
                   if (grepl("GnuTLS recv error [(]-9[)]", cond)){
                     m <- httr::POST(url=url, body=body,
                                          config=c(list(encoding='identity'), config))
-                    m <- list(content=charToRaw(m),
-                              status_code = "200",
-                              url=url)
-                    class(m) <- "response"
                     return(m)
                   }
                 })
