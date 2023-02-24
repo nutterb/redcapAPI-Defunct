1273deb463c82cd4d48ba996d2de947519eea231
Shawn Garbett <shawn@garbett.org>
2023-02-24 16:44:05 -0600

    Minor spacing changes

--- tests/testthat/test-exportArms.R
+++ tests/testthat/test-exportArms.R
@@ -2,8 +2,7 @@ context("exportArms")
 
 skip_on_cran()
 
-rcon <- redcapConnection(url = url,
-                         token = API_KEY)
+rcon <- redcapConnection(url = url, token = API_KEY)
 
 test_that("returns NULL for a simple project",{
   expect_null(exportArms(rcon))
