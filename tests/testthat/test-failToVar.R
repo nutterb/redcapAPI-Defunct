1d12d7a69b1049bad6276f68db115b93dbb9a7ee
Obregon, Savannah <savannah.obregon@vumc.org>
2023-02-28 10:34:30 -0600

    use numeric function to test

--- tests/testthat/test-failToVar.R
+++ tests/testthat/test-failToVar.R
@@ -9,20 +9,20 @@ test_that("date_ = as.Date returns class Date for date_mdy", expect_is(rec_hd$da
 test_that("date_ = as.Date returns class Date for date _ymd", expect_is(rec_hd$date_ymd, "Date"))
 
 # pull data for test time_ with handler
-rec_ht <- exportRecords(rcon, handlers = list(time_ = as.Date))
+rec_ht <- exportRecords(rcon, handlers = list(datetime_ = function(x) 1.23))
 test_that("time_ = as.Date returns class Date for time_hhmmss", expect_is(rec_ht$time_hhmmss, "Date")) # failing
 test_that("time_ = as.Date returns class Date for time_hhmm", expect_is(rec_ht$time_hhmm, "Date")) # failing
 test_that("time_ = as.Date returns class Date for time_mmss", expect_is(rec_ht$time_mmss, "Date")) # failing
 
 # pull data for test datetime_ with handler
-rec_hdt <- exportRecords(rcon, handlers=list(datetime_ = as.Date))
-test_that("datetime_ = as.Date returns class Date for datetime_dmy_hm", expect_is(rec_hdt$datetime_dmy_hm, "Date"))
-test_that("datetime_ = as.Date returns class Date for datetime_mdy_hm", expect_is(rec_hdt$datetime_mdy_hm, "Date"))
-test_that("datetime_ = as.Date returns class Date for datetime_ymd_hm", expect_is(rec_hdt$datetime_ymd_hm, "Date"))
+rec_hdt <- exportRecords(rcon, handlers=list(datetime_ = function(x) 1.23))
+test_that("datetime_ = as.Date returns class Date for datetime_dmy_hm", expect_is(rec_hdt$datetime_dmy_hm, "numeric"))
+test_that("datetime_ = as.Date returns class Date for datetime_mdy_hm", expect_is(rec_hdt$datetime_mdy_hm, "numeric"))
+test_that("datetime_ = as.Date returns class Date for datetime_ymd_hm", expect_is(rec_hdt$datetime_ymd_hm, "numeric"))
 
-test_that("datetime_ = as.Date returns class Date for datetime_dmy_hms", expect_is(rec_hdt$datetime_dmy_hms, "Date")) # failing
-test_that("datetime_ = as.Date returns class Date for datetime_mdy_hms", expect_is(rec_hdt$datetime_mdy_hms, "Date")) # failing
-test_that("datetime_ = as.Date returns class Date for datetime_ymd_hms", expect_is(rec_hdt$datetime_ymd_hms, "Date")) # failing
+test_that("datetime_ = as.Date returns class Date for datetime_dmy_hms", expect_is(rec_hdt$datetime_dmy_hms, "numeric")) # failing
+test_that("datetime_ = as.Date returns class Date for datetime_mdy_hms", expect_is(rec_hdt$datetime_mdy_hms, "numeric")) # failing
+test_that("datetime_ = as.Date returns class Date for datetime_ymd_hms", expect_is(rec_hdt$datetime_ymd_hms, "numeric")) # failing
 
 # pull data for test dates = true
 rec_dt <- exportRecords(rcon, dates = TRUE)
