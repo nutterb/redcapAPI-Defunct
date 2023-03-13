a5c701e274ad0309c518c5d96d4bcc839571829b
Shawn Garbett <shawn@garbett.org>
2023-03-13 15:33:44 -0500

    Project now has 2 arms

--- tests/testthat/test-exportArms.R
+++ tests/testthat/test-exportArms.R
@@ -2,6 +2,6 @@ context("exportArms")
 
 rcon <- redcapConnection(url = url, token = API_KEY)
 
-test_that("returns NULL for a simple project",{
-  expect_null(exportArms(rcon))
+test_that("returns 2 for a project with 2 arms",{
+  expect_equal(length(exportArms(rcon)), 2)
 })
